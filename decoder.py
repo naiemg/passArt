56e90a6e33f7cfb0081936b347eeab4208ca3ac7
naiemg <naiem.gafar@gmail.com>
2019-08-07 23:01:34 -0400

    encrypts image for security feature

--- decoder.py
+++ decoder.py
@@ -1,18 +1,19 @@
 from PIL import Image
 
-file1 = open("MyFile.txt","a")
+file1 = open("MyFile.txt","w", encoding="ascii")
 filename = 'encrypted.png'
 art = Image.open(filename)
 art = art.convert('RGB')
 width, height = art.size
+first_code = 126
+second_code = 55
 
 # Fix ranges to fit any picture
 for row in range(0, height, 10):
     for col in range(0, width, 10):
         color = art.getpixel((row+3, col+3))
         (r, g, b) = color
-        file1.write(str(chr(r))+str(chr(g))+str(chr(b)))
+        file1.write(str(chr((r-first_code%second_code)))+str(chr((g-first_code%second_code)))+str(chr((b-first_code%second_code))))
     file1.write("\n")
 
-#im.save(filename)
 file1.close()
--- drawArt.py
+++ drawArt.py
@@ -5,6 +5,8 @@ import string
 imageLength = 1000
 imageHeight = 1000
 filename = 'encrypted.png'
+random_shift = random.randint(1, 254)
+random_mod = random.randint(1, 100)
 
 def createCanvas():
     im = Image.new('RGB', (imageLength, imageHeight))
@@ -14,27 +16,29 @@ def generateRandomPW(stringLength):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(password_characters) for i in range(stringLength))
 
-def encodePassword(pw_input, level):
-    pw_split = list(pw_input)
+def encodePassword(pw_input, level):    # hello
+    #print("original: " + pw_input)
+    pw_split = list(pw_input)           # ['h','e','l','l','o']
     pw_numValues = []
 
     for char in pw_split:
-        pw_numValues.append(ord(char))
+        pw_numValues.append(ord(char))  # [104, 101, 108, 108, 111]
 
-    pw_color_vals = tuple(pw_numValues[x:x + 3]
-        for x in range(0, len(pw_numValues), 3))
+    pw_numValues_encrypted = []
+    for char in pw_split:               # [106, 103, 110, 110, 113]
+        pw_numValues_encrypted.append(ord(char)+random_shift%random_mod)
 
-    # For debugging purposes/ delete later
-    print(pw_split)
-    print(pw_numValues)
-    print(pw_color_vals)
+    pw_encoded = ''
+    for val in range(len(pw_numValues_encrypted)):
+        pw_encoded += chr(pw_numValues_encrypted[val])
+    #print("encoded: " + pw_encoded)
 
     im = Image.open(filename)
     draw = ImageDraw.Draw(im)
 
-    pw_numValues = pw_numValues *21
-    pw_color_vals = tuple(pw_numValues[x:x + 3]
-        for x in range(0, len(pw_numValues), 3))
+    pw_numValues_encrypted = pw_numValues_encrypted *21     # [106, 103, 110, 110, 113 ... ... ... ...]
+    pw_color_vals = tuple(pw_numValues_encrypted[x:x + 3]   # [(106, 103, 110),(110, 113 ...) ... ... ...]
+        for x in range(0, len(pw_numValues_encrypted), 3))
 
     for i in range(len(pw_color_vals)):
         draw.rectangle([(level, 10*i), (level+10, 10*i+10)], fill='rgb{}'.format(tuple(pw_color_vals[i])), outline='white', width=1)
@@ -54,3 +58,6 @@ while positionDown < imageHeight:
         randomNumber = random.randint(len(pw_input),32)
         encodePassword(generateRandomPW(randomNumber), positionDown)
     positionDown += 10   # decrease this value to add more layers
+
+print(random_shift)
+print(random_mod)
